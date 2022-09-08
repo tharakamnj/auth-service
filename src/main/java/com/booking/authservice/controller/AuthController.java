2760f747786bac15d946caffc9bf7dffbb916823
tharakamnj <trkamarathunga@gmail.com>
2022-09-08 21:34:01 +0530

    code modified

--- src/main/java/com/booking/authservice/controller/AuthController.java
+++ src/main/java/com/booking/authservice/controller/AuthController.java
@@ -5,12 +5,10 @@ import com.booking.authservice.dto.AuthRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     @Autowired
